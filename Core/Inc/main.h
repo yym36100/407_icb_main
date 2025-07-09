833fac973dd7ad18f800236847f439f283938b04
yym <yym@m.y>
2025-07-09 20:44:22 +0300

    buttons and leds from flash

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,18 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define KEY1_Pin GPIO_PIN_10
+#define KEY1_GPIO_Port GPIOE
+#define KEY2_Pin GPIO_PIN_11
+#define KEY2_GPIO_Port GPIOE
+#define KEY3_Pin GPIO_PIN_12
+#define KEY3_GPIO_Port GPIOE
+#define LED1_Pin GPIO_PIN_13
+#define LED1_GPIO_Port GPIOE
+#define LED2_Pin GPIO_PIN_14
+#define LED2_GPIO_Port GPIOE
+#define LED3_Pin GPIO_PIN_15
+#define LED3_GPIO_Port GPIOE
 
 /* USER CODE BEGIN Private defines */
 
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -95,9 +95,17 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int keys[3];
   while (1)
   {
     /* USER CODE END WHILE */
+	  keys[0] = HAL_GPIO_ReadPin(KEY1_GPIO_Port, KEY1_Pin);
+	  keys[1] = HAL_GPIO_ReadPin(KEY2_GPIO_Port, KEY2_Pin);
+	  keys[2] = HAL_GPIO_ReadPin(KEY3_GPIO_Port, KEY3_Pin);
+
+	  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, keys[0]);
+	  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, keys[1]);
+	  HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, keys[2]);
 
     /* USER CODE BEGIN 3 */
   }
@@ -189,13 +197,31 @@ static void MX_USART1_UART_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 /* USER CODE BEGIN MX_GPIO_Init_1 */
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, LED1_Pin|LED2_Pin|LED3_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : KEY1_Pin KEY2_Pin KEY3_Pin */
+  GPIO_InitStruct.Pin = KEY1_Pin|KEY2_Pin|KEY3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LED1_Pin LED2_Pin LED3_Pin */
+  GPIO_InitStruct.Pin = LED1_Pin|LED2_Pin|LED3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
